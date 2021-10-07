a7eb3ae45f7910fa51e266cb1cb5db6a87ee3899
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2021-10-07 19:22:26 +0100

    Update main.go

--- go/main.go
+++ go/main.go
@@ -9,5 +9,11 @@ points = 0
 fmt.Println("Loading HdIConsoleGO.. Setting Up")
 fmt.Println("")
 fmt.Println("Points loaded")
-
+fmt.Println("
+____       _       _
+|  _ \ ___ (_)_ __ | |_ ___  _
+| |_) / _ \| | '_ \| __/ __ (_)
+|  __/ (_) | | | | | |_\__  \_
+|_|   \___/|_|_| |_|\__|___ (_)
+")
 }
