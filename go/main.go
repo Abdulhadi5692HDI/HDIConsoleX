e34b847c9b8ba6f29d29712642dbc7d939667eed
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2021-10-07 19:19:09 +0100

    Update main.go

--- go/main.go
+++ go/main.go
@@ -1,6 +1,13 @@
 package main
+name = "HDIConsole"
+author = "__abdulhadi5692HDI__"
+version = 1
 import "fmt"
 
 func main() {
-fmt.Println("This is definintly no finished")
+points = 0
+fmt.Println("Loading HdIConsoleGO.. Setting Up")
+fmt.Println("")
+fmt.Println("Points loaded")
+
 }
