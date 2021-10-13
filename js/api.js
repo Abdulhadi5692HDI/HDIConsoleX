864fc9cf65d1e94b6ae92833d2b219f4ef147bcb
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2021-10-13 19:35:47 +0100

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,12 @@
+// An (fKIND)
+// of api
+
+var codes = [100, 200, 300, 2017];
+const h = 'h'
+const 'hadi' = 'start,1=h,2=a,3=d,4=i,end';
+
+function api() {
+console.log("Usage: api.<function>(par,int)");
+console.log("  %  ");
+console.log(" crash(<exit_par>); ");
+}
